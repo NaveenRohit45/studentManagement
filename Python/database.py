caf9b03dad04a0d352b4d7ab9d64e1537b9e574c
NaveenRohit45 <naveenk07pnk@gmail.com>
2022-09-03 01:42:28 +0530

    created modual

--- Python/database.py
+++ Python/database.py
@@ -1,19 +1,51 @@
 import sqlite3
 
-conn = sqlite3.connect('studentData.db')
 
-print("Opened database successfully")
+def connect_database():
+    conn = sqlite3.connect('DATABASE.db')
+    print("OPENED DATABASE SUCCESSFULLY\n")
+    return conn
+
+
+def creating_table():
+    conn = connect_database()
+    conn.execute('''CREATE TABLE IF NOT EXISTS STUDENT_DATABASE
+             (NAME  TEXT    NOT NULL,
+             AGE    INT     NOT NULL,
+             PHONE  INT     NOT NULL);''')
+    print("Table Created Successfully...\n")
+    return conn
 
 
 def insert_into_database(student_name, student_age, student_phone):
+    conn = connect_database()
+    print(student_name, student_age, student_phone)
+    conn.execute("INSERT INTO STUDENT_DATABASE (NAME,AGE,PHONE) \
+        VALUES (?,?,?)", (student_name, student_age, student_phone))
+
+    conn.commit()
+    print("INSERT THE STUDENT DETAILS SUCCESSFULLY")
 
 
+def delete_from_database(delete_name):
+    conn = creating_table()
+    conn.execute("DELETE FROM STUDENT_DATABASE WHERE NAME = ?", (delete_name,))
+    print("Student name has been deleted")
+    conn.commit()
 
 
-def delete_from_database():
-    check = searchIntoDatabase(student_delete)
-    while (FILE):
-        print("student $student_search_name has been deleted from database")
+def show_database():
+    conn = connect_database()
+    conn.execute("SELECT * FROM STUDENT_DATABASE")
+    output = conn.execute("SELECT * FROM STUDENT_DATABASE")
+    for row in output:
+        print(row)
+    conn.commit()
 
-def search_from_database():
 
+def search_from_database(search_name):
+    conn = connect_database()
+    conn.execute("SELECT * FROM STUDENT_DATABASE WHERE NAME = ?", (search_name,))
+    output = conn.execute("SELECT * FROM STUDENT_DATABASE WHERE NAME = ?", (search_name,))
+    print(output.fetchall())
+    conn.commit()
--- Python/home.py
+++ Python/home.py
@@ -1,46 +1,6 @@
-import time
-import main
-import os
-
-
-def header():
-    print('*****************')
-    print('Madras University')
-    print('*****************')
-
-
-def homepage():
-    print('1.Add Student Name')
-    print('2.Delete Student Name')
-    print('3.Search Student Name')
-    print('0.exit')
-
-
-def read_user_action():
-    user_action = int(input('Enter your action:'))
-    return user_action
-
-
-def process():
-    user_action = read_user_action()
-    if user_action == 1:
-        main.add_user()
-
-    elif user_action == 2:
-        main.delete_student_name()
-
-    elif user_action == 3:
-        main.searchStudentName()
-
-    elif user_action == 0:
-        print('THANK YOU..........!')
-        exit()
-
-    else:
-        print('Hey Hari')
-
+import menu
 
 if __name__ == '__main__':
-    homepage()
-    header()
-    process()
+    menu.homepage()
+    menu.header()
+    menu.process()
