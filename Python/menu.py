7f19acd0b469b6e78bcf018d3d3ae1ea51308d30
NaveenRohit45 <naveenk07pnk@gmail.com>
2022-09-03 01:44:09 +0530

    added menu.py

--- Python/menu.py
+++ Python/menu.py
@@ -1,6 +1,4 @@
-import time
 import database
-import os
 
 
 def header():
@@ -12,7 +10,9 @@ def header():
 def homepage():
     print('1.Add Student Name')
     print('2.Delete Student Name')
-    print('3.Search Student Name')
+    print('3.Search Student Details')
+    print('4.Show All Details')
+    print('5.Update Student Details')
     print('0.exit')
 
 
@@ -24,19 +24,24 @@ def read_user_action():
 def process():
     user_action = read_user_action()
     if user_action == 1:
+        header()
         add_user()
 
     elif user_action == 2:
-        database.delete_student_name()
+        header()
+        delete_student_name()
 
     elif user_action == 3:
-        database.searchStudentName()
+        header()
+        search_student_name()
 
     elif user_action == 4:
-        database.showDatabase()
+        header()
+        database.show_database()
 
     elif user_action == 5:
-        database.updateStudentDetails()
+        header()
+        updateStudentDetails()
 
     elif user_action == 0:
         print('THANK YOU..........!')
@@ -47,13 +52,17 @@ def process():
 
 
 def add_user():
-    print(student_name=input('Enter Student Name: '))
-    print(student_age=input('Enter Age of the Student: '))
-    print(student_phone=input('Enter Student Phone No: '))
-    database.insert_into_database(student_)
+    student_name = input('Enter Student Name: ')
+    student_age = input('Enter Age of the Student: ')
+    student_phone = input('Enter Student Phone No: ')
+    database.insert_into_database(student_name, student_age, student_phone)
 
 
-if __name__ == '__main__':
-    homepage()
-    header()
-    process()
+def delete_student_name():
+    delete_name = input('Enter the Name to delete Student Details: ')
+    database.delete_from_database(delete_name)
+
+
+def search_student_name():
+    search_name = input('ENTER THE STUDENT NAME: ')
+    database.search_from_database(search_name)
